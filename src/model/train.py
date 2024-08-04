5925a4465842800b8b4bc317e20e819b9d05c61d
speedwagon1299 <srihari5202004@gmail.com>
2024-08-04 23:06:07 +0530

    Challenge 3 Linted and Dusted

--- src/model/train.py
+++ src/model/train.py
@@ -43,7 +43,7 @@ def split_data(df):
     y = df['Diabetic'].values
 
     X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=0.2, random_state=42)
+                                                test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
 
