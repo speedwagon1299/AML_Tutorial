debc02f831154ccbed82e4f72e0e58496ec99ae1
speedwagon1299 <srihari5202004@gmail.com>
2024-08-04 23:07:41 +0530

    Challenge 3.4

--- src/model/train.py
+++ src/model/train.py
@@ -42,8 +42,9 @@ def split_data(df):
     X = df.drop(['Diabetic'], axis=1).values
     y = df['Diabetic'].values
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split \
+    (X, y,test_size=0.2, random_state=42)
+    
     return X_train, X_test, y_train, y_test
 
 
